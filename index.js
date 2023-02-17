db100f5a68a2697e4569074cfffd3c0e11be02fd
Ian Shehadeh <IanShehadeh2020@gmail.com>
2023-02-16 19:32:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const { Readability } = require('@mozilla/readability');
+const { JSDOM } = require('jsdom');
+const process = require('process');
+const Epub = require("epub-gen");
+const RSSParser = require('rss-parser');
+
+async function getArticle(url) {
+    const resp = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'string'
+    });
+    const dom = new JSDOM(resp.data);
+    const reader = new Readability(dom.window.document);
+    return reader.parse()
+}
+
+async function epubFromArticles(title, articles, path) {
+    let options = {
+        title,
+        author: "article2epub",
+        content: articles.map(a => ({ title: a.title, author: a.author, data: a.content }))
+    }
+
+    return new Epub(options, path)
+}
+
+
+async function rssToEpub(url, outPath) {
+    const feedParser = new RSSParser();
+    const feed = await feedParser.parseURL(url);
+    let articles = [];
+    for (const item of feed.items) {
+        try {
+            articles.push(await getArticle(item.link));
+        } catch (e) {
+            console.error(`failed to parse article '${item.title}': ${e}`);
+        }
+    }
+    return epubFromArticles(feed.title, articles, outPath)
+}
+
+rssToEpub(process.argv[2], process.argv[3]);
