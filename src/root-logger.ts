e1a74ccc5f921764381b3c219bdbb5d15907149e
Ian Shehadeh <ian@shehadeh.net>
2023-03-21 20:53:28 -0400

    set log level from env var

--- src/root-logger.ts
+++ src/root-logger.ts
@@ -1,7 +1,9 @@
+import { env } from "node:process";
 import pino from "pino";
 
 export const ROOT_LOGGER = pino({
     transport: {
         target: "pino-pretty",
     },
+    level: env["RSS2EPUB_LOG"],
 });
